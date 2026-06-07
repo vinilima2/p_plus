f67c01737fe3182ddaf94715d501b2cbe69f5eac
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2026-06-07 12:22:05 -0300

    feat: obter Gestor por ID

--- lib/services/gestor_service.dart
+++ lib/services/gestor_service.dart
@@ -3,4 +3,18 @@ import 'package:p_plus/dtos/gestor.dart';
 
 class GestorService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<Gestor?> obterGestor(String id) async {
+    try {
+      DocumentSnapshot snapshot = await _db.collection('gestor').doc(id).get();
+      if (snapshot.exists) {
+        return Gestor.fromFirestore(snapshot);
+      }
+      else {
+        return null;
+      }
+    } catch (error) {
+      throw "Erro ao obter Gestor: ${error.toString()}";
+    }
+  }
 }
